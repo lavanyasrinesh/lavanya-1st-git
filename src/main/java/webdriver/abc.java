011cd7054b57d392ecc81909b2c6e98826801359
lavanyasrinesh <lavan@LAPTOP-BPL6MCEE>
2021-05-19 23:16:20 -0500

    2nd commit

---
+++ src/main/java/webdriver/abc.java
@@ -0,0 +1,16 @@
+package webdriver;
+
+public class abc {
+
+	public static void main(String[] args) {
+		a1();
+
+	}
+	public  static void a1() {
+		int a=1,b=3,c;
+		c=a+b;
+		System.out.println(c);
+		
+	}
+
+}
